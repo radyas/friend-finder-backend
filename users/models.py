6d59e399d2fe8ff64b0c4869ca1a65f893e0848b
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2022-01-29 16:38:47 +0530

    added users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Roles(models.TextChoices):
+    # User Roles
+    SUPER_ADMIN = 'SUPER_ADMIN', _('SUPER_ADMIN')
+    HR = 'HR', _('HR')
+    EMPLOYEE = 'EMPLOYEE', _('EMPLOYEE')
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.EMPLOYEE
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def is_super_admin(self):
+        return self.role == Roles.SUPER_ADMIN or self.is_superuser
+
+    def is_admin_user(self):
+        return self.role in [Roles.HR, Roles.SUPER_ADMIN]
+
+    def is_user(self):
+        return self.role == Roles.EMPLOYEE
