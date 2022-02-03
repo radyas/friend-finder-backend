cc5aefbc40bd6fd05cb76344d7b9d5e36c5c3e69
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2022-02-03 09:17:30 +0530

    added rest framework to apps

--- ems_api/settings.py
+++ ems_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'files',
     'users',
 ]
@@ -140,9 +141,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
 
