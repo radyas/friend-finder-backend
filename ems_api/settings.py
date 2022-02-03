5663cf774d6b8b5dcd1b8f4f07516dadf5beca32
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2022-02-03 09:12:13 +0530

    added token expiry and rest framework config

--- ems_api/settings.py
+++ ems_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +134,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Test runner which will run the tests
 TEST_RUNNER = 'ems_api.pytest_runner.PytestTestRunner'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30)
+}
