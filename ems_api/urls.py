18288f6c49f0399122e2861f443883c05b7341ba
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2022-02-03 09:14:03 +0530

    added router urls

--- ems_api/urls.py
+++ ems_api/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from files.views import FileViewSet
@@ -28,4 +28,5 @@ router.register('files', FileViewSet, basename='files')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
