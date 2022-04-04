5e7ae48421ddecb8a09d6f3716297a53c81815fe
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2022-04-04 10:15:59 +0530

    added heroku config

--- friend_finder/settings.py
+++ friend_finder/settings.py
@@ -13,6 +13,9 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+import django_heroku
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
