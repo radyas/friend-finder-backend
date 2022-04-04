0f9a542783419270957dd64098b195ea0de1f6db
Yasiru Sellahewa <yasirusellahewa@gmail.com>
2022-04-04 11:45:38 +0530

    added heroku config

--- friend_finder/settings.py
+++ friend_finder/settings.py
@@ -13,6 +13,8 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,7 +123,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "static"
-print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -153,3 +154,6 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30)
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
